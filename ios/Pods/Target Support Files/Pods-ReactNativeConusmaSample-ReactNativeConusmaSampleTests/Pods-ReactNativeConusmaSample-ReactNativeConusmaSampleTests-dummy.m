24062ff33d52cfa8008d0245f76e52e08c3c8b4c
hdoru <huseyindoru01@gmail.com>
2021-05-27 16:04:17 +0300

    delete

--- ios/Pods/Target Support Files/Pods-ReactNativeConusmaSample-ReactNativeConusmaSampleTests/Pods-ReactNativeConusmaSample-ReactNativeConusmaSampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeConusmaSample_ReactNativeConusmaSampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeConusmaSample_ReactNativeConusmaSampleTests
-@end
