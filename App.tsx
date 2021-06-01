98d6d8804ab6e5adafa44984ac5a267b8c4c34df
Murat Demirci <mdemirci01@gmail.com>
2021-06-01 13:22:58 +0300

    started to enableAudioVideo with RTCView

--- App.tsx
+++ App.tsx
@@ -7,109 +7,57 @@
  *
  * @format
  */
-
- import React from 'react';
- import {
+import Conusma from 'react-native-conusma';
+import React, {useState} from 'react';
+import {
    SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
+   Button,
    View,
+   StyleSheet
  } from 'react-native';
-
  import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
-
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+  RTCView,
+  MediaStream
+} from 'react-native-webrtc';
 
  const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+   var conusma = new Conusma();
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
-
+   const [localStream, setLocalStream] = useState<MediaStream>();
+   const startLocalStream = async () => {
+     const newStream:any = await conusma.enableAudioVideo();
+     setLocalStream(newStream);
+   }
    return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="CONUSMA">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="CONUSMA">
-             <ReloadInstructions />
-           </Section>
-           <Section title="CONUSMA">
-             <DebugInstructions />
-           </Section>
-           <Section title="CONUSMA">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+    <SafeAreaView style={styles.container}>
+      <Button title="Kamerayı aç ve akışa başla" onPress={startLocalStream} />
+      <View style={styles.rtcview}>
+        {localStream && (
+          <RTCView style={styles.rtc} streamURL={localStream.toURL()} />
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#333',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+  },
+  rtcview: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'black',
+  },
+  rtc: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
  export default App;
