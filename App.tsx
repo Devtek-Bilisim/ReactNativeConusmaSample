5223df24c54b28e607e9787288264ca6a3264374
hdoru <huseyindoru01@gmail.com>
2021-06-09 16:58:26 +0300

    update

--- App.tsx
+++ App.tsx
@@ -1,69 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-import Conusma from 'react-native-conusma';
-import React, {useState} from 'react';
-import {
-   SafeAreaView,
-   Button,
-   View,
-   StyleSheet,
-   Alert
- } from 'react-native';
- import {
-  RTCView,
-  MediaStream
-} from 'react-native-webrtc';
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import AppContainer from './src/navigation/AppContainer';
 
- const App = () => {
-   var conusma = new Conusma("a2bdd634-4cf3-4add-9834-d938f626dd20", { apiUrl: "https://emscloudapi.com:7788" });
-
-   const [localStream, setLocalStream] = useState<MediaStream>();
-   const startLocalStream = async () => {
-    var user = await conusma.createUser();
-    var meeting = await user.getProfileMeeting();
-    console.log("meeting => "+JSON.stringify(meeting));
-    var activeMeeting = await user.joinMeeting(meeting);
-    activeMeeting.open();
-    console.log(user.userInfo);
-
-   }
-   return (
-    <SafeAreaView style={styles.container}>
-      <Button title="Kamerayı aç ve akışa başla" onPress={startLocalStream} />
-      <View style={styles.rtcview}>
-        {localStream && (
-          <RTCView style={styles.rtc} streamURL={localStream.toURL()} />
-        )}
-      </View>
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <AppContainer />
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#333',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '100%',
-  },
-  rtcview: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
-    width: '100%',
-    backgroundColor: 'black',
-  },
-  rtc: {
-    width: '100%',
-    height: '100%',
-  },
-});
-
- export default App;
+export default App;
