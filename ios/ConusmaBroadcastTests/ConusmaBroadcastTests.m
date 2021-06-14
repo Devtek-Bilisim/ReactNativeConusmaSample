545ca6324a532d6d1cb1aa733f781eb5a93bd3c4
hdoru <huseyindoru01@gmail.com>
2021-06-14 13:16:43 +0300

    rename app

--- ios/ConusmaBroadcastTests/ConusmaBroadcastTests.m
+++ ios/ConusmaBroadcastTests/ConusmaBroadcastTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeConusmaSampleTests : XCTestCase
+@interface ConusmaBroadcastTests : XCTestCase
 
 @end
 
-@implementation ReactNativeConusmaSampleTests
+@implementation ConusmaBroadcastTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
